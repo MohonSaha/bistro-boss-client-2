bc643a3bd4bf156d37a11465bff7f1e0e3da6cde
MohonSaha <practicewithmohon@gmail.com>
2023-06-03 00:22:48 +0600

    implement axios interceptor

---
+++ src/hooks/useAdmin.jsx
@@ -0,0 +1,8 @@
+// import useAuth from "./useAuth";
+
+
+// const useAdmin = () => {
+//    const {user} = useAuth()
+// };
+
+// export default useAdmin;
--- src/hooks/useCart.jsx
+++ src/hooks/useCart.jsx
@@ -1,20 +1,32 @@
 import { useQuery } from '@tanstack/react-query'
-import { useContext } from 'react';
-import { AuthContext } from '../providers/authProvider';
+import useAuth from './useAuth';
+import useAxiosSecure from './useAxiosSecure';
 
 
 const useCart = () => {
-    const { user } = useContext(AuthContext)
-    const token = localStorage.getItem('access-token')
+    const { user } = useAuth()
+    // const token = localStorage.getItem('access-token');
+    const [axiosSecure] = useAxiosSecure()
+
+
 
     const { refetch, data: cart = [] } = useQuery({
         queryKey: ['carts', user?.email],
+        // queryFn: async () =>{
+        //     const res = await fetch(`http://localhost:5000/carts?email=${user?.email}`, {headers: {
+        //         authorization: `bearer ${token}`
+        //     }})
+        //     return res.json()
+        // },
+
+
+        
         queryFn: async () =>{
-            const res = await fetch(`http://localhost:5000/carts?email=${user?.email}`, {headers: {
-                authorization: `bearer ${token}`
-            }})
-            return res.json()
+            const res = await  axiosSecure(`/carts?email=${user?.email}`)
+            console.log('Response From axios', res);
+            return res.data;
         },
+
     })
     return [cart, refetch]
 
