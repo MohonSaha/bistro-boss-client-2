53a99df9884228614579f5aa01f9aa4d2c62c458
MohonSaha <practicewithmohon@gmail.com>
2023-06-02 14:39:17 +0600

    use jwt

--- src/Pages/Dashboard/AllUsers/AllUsers.jsx
+++ src/Pages/Dashboard/AllUsers/AllUsers.jsx
@@ -1,6 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 import TitlePage from "../../Shared/TitlePage/TitlePage";
 import { FaTrashAlt, FaUserShield } from "react-icons/fa";
+import Swal from "sweetalert2";
 
 
 const AllUsers = () => {
@@ -12,6 +13,26 @@ const AllUsers = () => {
 
     const handleDelete = (user) => {
         console.log(user);
+    }
+
+
+    const handleMakeAdmin = (user) => {
+        fetch(`http://localhost:5000/users/admin/${user._id}`, {
+            method: 'PATCH'
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                if (data.modifiedCount) {
+                    refetch();
+                    Swal.fire({
+                        icon: 'success',
+                        title: `${user.name} is an Admin now.`,
+                        showConfirmButton: false,
+                        timer: 1500
+                    })
+                }
+            })
     }
 
 
@@ -36,16 +57,16 @@ const AllUsers = () => {
                     <tbody>
 
                         {
-                            users.map((user, index) => <tr key={user._id}>
+                            users.map((user, index) => <tr key={user}>
                                 <th>{index + 1}</th>
                                 <td>{user.name}</td>
                                 <td>{user.email}</td>
-                                <td>{user.role === 'admin' ? 'admin' : 
-                                <button onClick={() => handleDelete(user)} className="btn btn-ghost hover:text-orange-500 bg-orange-500 text-xl btn-circle text-white"><FaUserShield></FaUserShield></button>}
+                                <td>{user.role === 'admin' ? 'admin' :
+                                    <button onClick={() => handleMakeAdmin(user)} className="btn btn-ghost hover:text-orange-500 bg-orange-500 text-xl btn-circle text-white"><FaUserShield></FaUserShield></button>}
 
                                 </td>
                                 <td>
-                                    <button onClick={() => handleDelete(user)} className="btn btn-ghost hover:text-red-500 bg-red-500 btn-circle text-white"><FaTrashAlt></FaTrashAlt></button>
+                                    <button onClick={() => handleDelete(user._id)} className="btn btn-ghost hover:text-red-500 bg-red-500 btn-circle text-white"><FaTrashAlt></FaTrashAlt></button>
                                 </td>
                             </tr>)
                         }
--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -2,6 +2,7 @@ import { createContext, useEffect, useState } from "react";
 import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
 import { app } from "../firebase/firebase.config";
 import { GoogleAuthProvider } from "firebase/auth";
+import axios from "axios";
 
 
 export const AuthContext = createContext(null)
@@ -49,6 +50,24 @@ const AuthProvider = ({children}) => {
        const unsubscribe = onAuthStateChanged(auth, currentUser =>{
             setUser(currentUser)
             console.log('current user', currentUser);
+
+            // get and set token:-
+            // I have use here axios js instade of fetch
+            
+            if(currentUser){
+                axios.post('http://localhost:5000/jwt', {email: currentUser.email})
+                .then(data => {
+                    console.log(data.data.token);
+                    localStorage.setItem('access-token', data.data.token)
+                })
+            }
+            else{
+                localStorage.removeItem('access-token')
+            }
+
+
+
+
             setLoading(false)
         });
         return ()=>{
